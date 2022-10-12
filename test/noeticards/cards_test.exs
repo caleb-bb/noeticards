3e2a09dc13907ed18d74be526017b8256c784edb
Caleb <beerscb@gmail.com>
2022-10-12 13:36:46 -0400

    add tests

--- test/noeticards/cards_test.exs
+++ test/noeticards/cards_test.exs
@@ -43,4 +43,34 @@ defmodule Noeticards.CardsTest do
              } = errors_on(changeset)
     end
   end
+
+  describe "cards_in_deck!/1" do
+    test "it returns all cards with the correct deck id" do
+      first_deck = insert(:deck)
+      second_deck = insert(:deck)
+
+      insert_this_many(3, :card, %{deck: first_deck})
+      insert_this_many(7, :card, %{deck: second_deck})
+
+      cards_in_first_deck = Cards.cards_in_deck!(first_deck.id)
+      cards_in_second_deck = Cards.cards_in_deck!(second_deck.id)
+
+      assert Repo.preload(first_deck, :cards).cards == cards_in_first_deck
+      assert Repo.preload(second_deck, :cards).cards == cards_in_second_deck
+    end
+
+    test "it returns an empty list for nonexistent or empty deck" do
+      empty_deck = insert(:deck)
+
+      assert Cards.cards_in_deck!(empty_deck.id) == []
+      assert Cards.cards_in_deck!(7327) == []
+    end
+  end
+
+  def insert_this_many(how_many, thing_to_insert, params \\ %{})
+      when how_many >= 1 and thing_to_insert in [:card, :deck] do
+    for _count <- 1..how_many do
+      insert(thing_to_insert, params)
+    end
+  end
 end
